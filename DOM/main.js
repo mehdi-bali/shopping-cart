a9bf60be7a257482e2f990579c1acf1241d33fdd
mehdi-bali <66732755+mehdi-bali@users.noreply.github.com>
2020-07-04 15:30:32 +0100

    Add files via upload

---
+++ DOM/main.js
@@ -0,0 +1,19 @@
+//Variables Globales
+
+const buttonlike = Array.from(document.querySelectorAll(".button-like"));
+
+
+
+//Change Color Function
+
+function changecolor (el){
+
+	if (el.target.style.color !=== "red"){
+
+		el.target.style.color === "red":
+
+	} else {(el.target.style.color === "black";}
+
+}
+
+		
